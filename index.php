67284b286125cb62d4271f0e1d160153b81ed170
BlackSky971 <yoyosbh971@gmail.com>
2025-01-10 16:12:06 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Modèle de page : Accueil
+ * Description : Modèle de page pour l'accueil du site.
+ */
+
+ <?php get_header(); ?>
+ <h1>Bienvenue sur mon thème personnalisé</h1>
+ <?php get_footer(); ?>
+ 
